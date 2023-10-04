09e4be417a386a825da332b1718cd611f060d532
25Shawn <2238577@etudiant.cegepvicto.ca>
2023-10-04 19:10:59 -0400

    premier commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apical</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="https://apical.xyz/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://apical.xyz/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://apical.xyz/favicon-16x16.png">
+    <link rel="stylesheet" href="/wp-content/themes/dev2/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e538d494e4.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    
+    <header id="container_principale">
+        
+        <div id="logo">
+        
+            <div class="logo_principale">
+                <img id="image_apical" src="https://apical.xyz/medias/fr/LogoApical-blanc.svg" alt="Apical, ma plateforme d'apprentissage" />
+            </div>
+            <div class="menuicones">
+                <a href="https://apical.xyz" class="icone"><img src="https://apical.xyz/medias/commun/Accueil-MenuSecondaire.svg" alt="Accueil" title="Accueil" /></a>
+                <a href="#" class="icone ouvrirpopuprecherche" data-target="#popuprecherche"><img src="https://apical.xyz/medias/commun/Rechercher-MenuSecondaire.svg" alt="Recherche" title="Rechercher"/></a>
+                <a href="#" class="icone ouvrirpopupauthentification" data-target="#popupauthentification"><img src="https://apical.xyz/medias/commun/Login-MenuSecondaire.svg" alt="Authentification" title="" /></a>
+            </div>
+        </div>
+        
+
+
+        <ul class="nav_bar_principale">
+
+                
+
+            <li class="nav_lien">
+                <a href="https://apical.xyz/formations" class=" nav-link text-uppercase text-expanded active">FORMATIONS</a>
+            </li>
+
+            <li class="nav_lien">
+                <a href="https://apical.xyz/articles" class="nav-link text-uppercase text-expanded ">BLOGUE</a>
+            </li>
+
+
+            <li class="nav_lien dropdown">
+                <a href="https://apical.xyz/pages/formulairebcrypt" class="dropdown-toggle nav-link text-uppercase text-expanded">OUTILS <i class="fa-solid fa-caret-up fa-rotate-180"></i></a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item " href="https://apical.xyz/pages/formulairebcrypt">Hachage bcrypt </a>
+                    <a class="dropdown-item " href="https://apical.xyz/pages/aleatoire">Générateur aléatoire </a>
+                    <a class="dropdown-item " href="https://apical.xyz/pages/fontAwesome">Icônes Font Awesome </a>
+                </div>
+            </li>
+
+            <li class="nav_lien dropdown">
+                <a href="https://apical.xyz/contact" class="dropdown-toggle nav-link text-uppercase text-expanded ">AIDE <i class="fa-solid fa-caret-up fa-rotate-180"></i></a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item " href="https://apical.xyz/contact">Contact </a>
+                    <a class="dropdown-item " href="https://apical.xyz/apropos">À propos </a>
+                </div>
+            </li>
+
+        </ul>
+    </header>
+    
+</body>
+</html>
