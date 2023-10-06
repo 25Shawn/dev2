be0d33ec8ac3748a67ac2e3cd30969caba5ce2b0
25Shawn <2238577@etudiant.cegepvicto.ca>
2023-10-06 14:03:50 -0400

    deuxieme commit

--- index.php
+++ index.php
@@ -8,32 +8,51 @@
     <link rel="icon" type="image/png" sizes="32x32" href="https://apical.xyz/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://apical.xyz/favicon-16x16.png">
     <link rel="stylesheet" href="/wp-content/themes/dev2/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Homemade+Apple&family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
     <script src="https://kit.fontawesome.com/e538d494e4.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
     
-    <header id="container_principale">
+    <header id="container_header_principale">
         
         <div id="logo">
         
             <div class="logo_principale">
                 <img id="image_apical" src="https://apical.xyz/medias/fr/LogoApical-blanc.svg" alt="Apical, ma plateforme d'apprentissage" />
             </div>
-            <div class="menuicones">
-                <a href="https://apical.xyz" class="icone"><img src="https://apical.xyz/medias/commun/Accueil-MenuSecondaire.svg" alt="Accueil" title="Accueil" /></a>
-                <a href="#" class="icone ouvrirpopuprecherche" data-target="#popuprecherche"><img src="https://apical.xyz/medias/commun/Rechercher-MenuSecondaire.svg" alt="Recherche" title="Rechercher"/></a>
-                <a href="#" class="icone ouvrirpopupauthentification" data-target="#popupauthentification"><img src="https://apical.xyz/medias/commun/Login-MenuSecondaire.svg" alt="Authentification" title="" /></a>
+
+            <div id="nav_bar_icone">
+                <div class="menuicones">
+
+                    <div class=" dropdown">
+                        <a href="https://apical.xyz" class="icone ouvrirpopuprecherche"><img src="https://apical.xyz/medias/commun/Accueil-MenuSecondaire.svg" alt="Accueil"/></a>
+                        <div class="dropdown-menu">
+                            <a href="https://apical.xyz" class="dropdown-item ">Accueil</a>
+                        </div>
+                    </div>
+
+
+                    <div class=" dropdown">
+                        <a href="#" class="icone ouvrirpopuprecherche" data-target="#popuprecherche"><img src="https://apical.xyz/medias/commun/Rechercher-MenuSecondaire.svg" alt="Recherche"/></a>
+                        <div class="dropdown-menu">
+                            <a href="https://apical.xyz" class="dropdown-item ">Rechercher</a>
+                        </div>
+                    </div>
+
+                    <div class=" dropdown">
+                        <a href="#" class="icone ouvrirpopupauthentification" data-target="#popupauthentification"><img src="https://apical.xyz/medias/commun/Login-MenuSecondaire.svg" alt="Authentification" title="" /></a>
+                    </div>
+                </div>
             </div>
+            
         </div>
         
 
 
         <ul class="nav_bar_principale">
 
-                
 
             <li class="nav_lien">
                 <a href="https://apical.xyz/formations" class=" nav-link text-uppercase text-expanded active">FORMATIONS</a>
@@ -64,5 +83,40 @@
         </ul>
     </header>
     
+    <hr id="hr_premier">
+
+    <div class="publicite baspage">
+        <div class="pubhebergement">
+            <p>Site fièrement hébergé chez <a href="http://www.a2hosting.com?aid=5ca65a17be949" target="_top" id="hosting">A2 Hosting</a>.</p>
+            <p><a href="https://www.a2hosting.com?aid=5ca65a17be949&amp;bid=ed1c4a67" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/banners/ed1c4a67.jpg" alt="" title="" width="728" height="90" /></a><img style="border:0" src="https://affiliates.a2hosting.com/scripts/imp.php?aid=5ca65a17be949&amp;bid=ed1c4a67" width="1" height="1" alt="" /></p>
+        </div>
+    </div>
+    
+
+    <hr>
+    <footer id="container_footer_principale">
+
+        <div id="sous_container_footer">
+
+            <div class="container_footer">
+                <div class="row">
+                    <div class="col text-center">© 2023 Tous droits réservés.</div>
+                </div>
+            </div>
+            <div class="container_footer">
+                <div class="row">
+                    <div class="col text-center">Conception et développement de la plateforme : <span class="auteur"><a href="https://christianelagace.com" target="_blank">Christiane Lagacé</a></span></div>
+                </div>
+            </div>
+            <div class="container_footer">
+                <div class="row">
+                    <div class="col text-center mt-3">
+                        <a class="btn btn-primary" href="https://apical.xyz/contact?de=https%3A%2F%2Fapical.xyz%2Fformations%2Fphp">Écrivez-moi&nbsp;!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
 </body>
 </html>
